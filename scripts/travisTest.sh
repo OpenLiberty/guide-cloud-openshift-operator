44b354350ee599fd8d1112a37d04f66504c3d1b9
Mai Hameed <maihameed@hotmail.com>
2020-05-21 13:41:45 -0400

    fixing tests

--- scripts/travisTest.sh
+++ scripts/travisTest.sh
@@ -7,16 +7,35 @@ set -euxo pipefail
 ##
 ##############################################################################
 
+# Package the system/ and inventory/ apps
 mvn -q -pl models install
 mvn -q package
 
+# Logging into the OKD cluster as admin + creating a project named 'guide'
 oc login -u system:admin
 oc new-project guide
 
+# Fetch the latest release version of the Open Liberty Operator
+LATEST_VERSION=$(curl -s https://api.github.com/repos/OpenLiberty/open-liberty-operator/releases/latest | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/' | cut -c 2-)
+printf $LATEST_VERSION
+
+# Installing the OL Operator to the OKD cluster
+oc apply -f https://raw.githubusercontent.com/OpenLiberty/open-liberty-operator/master/deploy/releases/$LATEST_VERSION/openliberty-app-crd.yaml
+OPERATOR_NAMESPACE=guide
+WATCH_NAMESPACE=guide
+curl -L https://raw.githubusercontent.com/OpenLiberty/open-liberty-operator/master/deploy/releases/$LATEST_VERSION/openliberty-app-operator.yaml \
+      | sed -e "s/OPEN_LIBERTY_WATCH_NAMESPACE/${WATCH_NAMESPACE}/" \
+      | oc apply -n ${OPERATOR_NAMESPACE} -f -
+
+# View the installed resources
+oc api-resources --api-group=openliberty.io
+
+# Deploy zookeeper straight from DockerHub
 oc new-app bitnami/zookeeper:3 \
   -l name=kafka \
   -e ALLOW_ANONYMOUS_LOGIN=yes
 
+# Deploy Kafka straight from DockerHub
 oc new-app bitnami/kafka:2 \
   -l name=kafka \
   -e KAFKA_CFG_ZOOKEEPER_CONNECT=zookeeper:2181 \
