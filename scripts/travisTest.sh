1e629bdde5756f6d1f9addcaacf00fb38a8c95ee
Mai Hameed <maihameed@hotmail.com>
2020-05-21 12:08:48 -0400

    removing my branch from travis tests

---
+++ scripts/travisTest.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -euxo pipefail
+
+##############################################################################
+##
+##  Travis CI test script
+##
+##############################################################################
+
+mvn -q package
+
+oc registry login --skip-check=true
+
+docker build -t `oc registry info`/`oc project -q`/system:test system/.
+docker build -t `oc registry info`/`oc project -q`/inventory:test inventory/.
+
+oc apply -f ../scripts/test.yaml
+
+sleep 60
+
+oc get pods
+
+oc describe pods
+
+oc get routes
+
+SYSTEM_IP=`oc get route system-route -o=jsonpath='{.spec.host}'`
+INVENTORY_IP=`oc get route inventory-route -o=jsonpath='{.spec.host}'`
+
+curl http://$SYSTEM_IP/system/properties
+curl http://$INVENTORY_IP/inventory/systems/system-service
+
+mvn verify -Ddockerfile.skip=true -Dsystem.ip=$SYSTEM_IP -Dinventory.ip=$INVENTORY_IP
+
+oc logs $(oc get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep system)
+oc logs $(oc get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep inventory)
