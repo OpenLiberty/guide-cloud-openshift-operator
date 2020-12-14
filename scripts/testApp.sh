b3a8ed6be527b27a6a8539165c6185d9ffe9a927
Austin Seto <austin.seto@ibm.com>
2020-12-14 16:57:14 -0500

    clean package

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 # Package the system/ and inventory/ apps
 mvn -q -pl models install
-mvn -q package
+mvn -q clean package
 
 # Verifies that the system/inventory apps are functional
 mvn -pl system verify
