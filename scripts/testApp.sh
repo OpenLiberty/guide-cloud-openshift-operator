df3d5e7a5be6d7bc57d5aac34b3b4896eed77c3b
Austin Seto <austin.seto@ibm.com>
2020-12-14 17:06:11 -0500

    Get error stack traces

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -2,12 +2,12 @@
 set -euxo pipefail
 
 # Package the system/ and inventory/ apps
-mvn -q -pl models install
-mvn -q clean package
+mvn -q -pl models install -e
+mvn -q clean package -e
 
 # Verifies that the system/inventory apps are functional
-mvn -pl system verify
-mvn -pl inventory verify
+mvn -pl system verify -e
+mvn -pl inventory verify -e
 
 # Delete m2 cache after completion
 rm -rf ~/.m2
