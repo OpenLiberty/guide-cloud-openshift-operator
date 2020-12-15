fb2cdd65ad486dcc0ab97a2374249304d060f0d6
Austin Seto <austin.seto@ibm.com>
2020-12-15 09:20:31 -0500

    Minor change to maven commands

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -2,12 +2,12 @@
 set -euxo pipefail
 
 # Package the system/ and inventory/ apps
-mvn -q -pl models install -e
-mvn -q clean package -e
+mvn -q install
+mvn -q clean package
 
 # Verifies that the system/inventory apps are functional
-mvn -pl system verify -e
-mvn -pl inventory verify -e
+mvn -pl system verify
+mvn -pl inventory verify
 
 # Delete m2 cache after completion
 rm -rf ~/.m2
