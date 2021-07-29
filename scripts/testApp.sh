d0b2b69c77c21fb5b9b43a5ff218555b5d23f9ce
Austin Seto <austinseto@gmail.com>
2021-07-29 15:26:10 -0400

    Update testApp.sh

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -3,7 +3,10 @@ set -euxo pipefail
 
 # Package the system/ and inventory/ apps
 mvn -q -pl models install
-mvn -q clean package
+mvn -Dhttp.keepAlive=false \
+    -Dmaven.wagon.http.pool=false \
+    -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
+    -q clean package
 
 # Verifies that the system/inventory apps are functional
 mvn -pl system verify
