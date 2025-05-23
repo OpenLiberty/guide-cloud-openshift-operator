b12f80d2eef481358a72d9c6c8208588269055ae
Andrew Sasmito <Andrew.Sasmito@ibm.com>
2025-05-23 16:49:15 -0400

    Update the order of imports
    Signed-off-by: Andrew Sasmito <Andrew.Sasmito@ibm.com>

--- finish/inventory/src/test/java/it/io/openliberty/guides/inventory/InventoryServiceIT.java
+++ finish/inventory/src/test/java/it/io/openliberty/guides/inventory/InventoryServiceIT.java
@@ -37,6 +37,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.GenericContainer;
 // end::KafkaProducer[]
+
 import org.testcontainers.containers.Network;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
