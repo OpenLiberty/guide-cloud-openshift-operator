4b68f46785c0ec7951032a9169f8cb81d9a433ba
Andrew Sasmito <98786007+AndrewSasmito@users.noreply.github.com>
2025-05-27 14:13:11 -0400

    Update InventoryServiceIT.java

--- finish/inventory/src/test/java/it/io/openliberty/guides/inventory/InventoryServiceIT.java
+++ finish/inventory/src/test/java/it/io/openliberty/guides/inventory/InventoryServiceIT.java
@@ -35,7 +35,7 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.GenericContainer;
-// tag::KafkaProducer[]
+// end::KafkaProducer[]
 import org.testcontainers.containers.Network;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
